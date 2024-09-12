d1339d7b182872623e5b32879dee23d9276528c0
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2024-09-12 14:53:44 +0400

    Status thumbs 2

--- Models/StatusDataModel.cs
+++ Models/StatusDataModel.cs
@@ -10,17 +10,17 @@ namespace ChatApp.Models
     {
         public string ContactName { get; set; }
         public Uri ContactPhoto { get; set; }
+        public Uri StatusImage { get; set; }
+
+        // If we want to add our status
+        public bool IsMeAddStatus { get; set; }
+
 
         /// <summary>
         /// We will be covering in one of our upcoming videos
         /// TO-DO: Status Message
         /// </summary>
         //public string StatusMessage { get; set; }
-        public Uri StatusImage { get; set; }
-
-        // If we want to add our status
-        public bool IsMeAddStatus { get; set; }
-
 
     }
 }
--- ViewModels/ViewModel.cs
+++ ViewModels/ViewModel.cs
@@ -11,20 +11,40 @@ namespace ChatApp.ViewModels
 {
     public class ViewModel
     {
-        ObservableCollection<StatusDataModel> statusThumbsCollection;
+        public ObservableCollection<StatusDataModel> statusThumbsCollection;
 
-        ViewModel() 
+        public ViewModel() 
         {
+
+            // Lets bind our collection to itemscontrol
             statusThumbsCollection = new ObservableCollection<StatusDataModel>()
             {
+                // Since we want to keep first status blank for the user to add own status
                 new StatusDataModel()
                 {
-                    ContactName="",
-                    //ContactPhoto="",
-                    //StatusImage ="",
-
-                    //IsMeAddStatus=""
-
+                    IsMeAddStatus=true
+                }, 
+                
+                new StatusDataModel()
+                {
+                    ContactName="Elgun",
+                    ContactPhoto=new Uri("/assets/1.png", UriKind.RelativeOrAbsolute),
+                    StatusImage =new Uri("/assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus=false
+                },
+                new StatusDataModel()
+                {
+                    ContactName="Shamkhal",
+                    ContactPhoto=new Uri("/assets/2.jpg", UriKind.RelativeOrAbsolute),
+                    StatusImage =new Uri("/assets/8.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus=false
+                },
+                new StatusDataModel()
+                {
+                    ContactName="Lala",
+                    ContactPhoto=new Uri("/assets/3.png", UriKind.RelativeOrAbsolute),
+                    StatusImage =new Uri("/assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus=false
                 },
             };
         }
